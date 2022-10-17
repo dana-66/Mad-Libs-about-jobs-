f712322682868684da3b78920d0e0ae07f25b3e0
dana-66 <88723929+dana-66@users.noreply.github.com>
2022-10-17 21:24:53 +0300

    Create madLibs.py

---
+++ madLibs.py
@@ -0,0 +1,16 @@
+# MAdLibs is u take a random words and then add them to a story
+
+#getting input from user
+occupation = input("Enter a occupation: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjec = input("Enter an adjective: ")
+verb2 = input("Enter another verb: ")
+noun2 = input("Enter another noun: ")
+
+#Displaying the output
+print(f"\nToday a {occupation} named {noun} came to our school to talk to us about her job. "
+      f"/nShe said the most important skill you need to know to do her job is to be able to {verb} around (a) {adjec}{ noun}."
+      f"\nShe said it was easy for her to learn her job because she loved to {verb2} when she was my age--and that helps a lot!"
+      f"\nIf you're considering her profession, I hope you can be near (a) {adjec} {noun}."
+      f"\nThat's very important! If you get too distracted in that situation you won't be able to {verb} next to (a) {noun2}!")
